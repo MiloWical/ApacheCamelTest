c0c4a8360b2ff5f384d7682dce4dc2f5a376bbcb
Milo Wical <milo.wical@gmail.com>
2022-06-10 21:36:06 -0400

    New shutdown mechanism.

--- apachecameltest/src/main/java/git/milowical/App.java
+++ apachecameltest/src/main/java/git/milowical/App.java
@@ -1,30 +1,49 @@
 package git.milowical;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class App {
   public static void main(String[] args) {
-    
+
     CamelRouteConfiguration camelRouteConfiguration = new CamelRouteConfiguration(args[0], args[1]);
 
     CamelContext context = new DefaultCamelContext();
+
+    setupShutdownHook(context);
 
     try {
       context.addRoutes(new KafkaWriterRouter(camelRouteConfiguration));
       context.addRoutes(new PulsarReaderRouter(context, camelRouteConfiguration));
 
       context.start();
-
-      System.in.read();
-    } 
-    catch (Exception e) {
+    } catch (Exception e) {
       System.out.println(e);
       Util.printStackTrace(e); // Use this to get the full stack trace, not just a summarization.
     }
-
-    context.stop();
   }
 
-  
+  private static void setupShutdownHook(CamelContext context) {
+    Thread shutdownListener = new Thread() {
+      public void run() {
+        try {
+          System.out.println("><><>< CLOSING CAMEL CONTEXT ><><><");
+          context.close();
+        } catch (Exception e) {
+          System.out.println(e);
+          Util.printStackTrace(e); // Use this to get the full stack trace, not just a summarization.
+        }
+      }
+    };
+
+    Runtime.getRuntime().addShutdownHook(shutdownListener);
+
+    // No-op to keep a thread running until SIGTERM
+    Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {}, 0, 2,
+        TimeUnit.SECONDS);
+
+  }
 }
